6dc9445b2935e59ea4fce3506f10b525d66ebd2f
Jihani-31 <jihaninanayakkara@gmail.com>
2020-05-21 10:17:14 +0530

    First Commit

---
+++ Assignment1/src/jihani/function.scala
@@ -0,0 +1,36 @@
+package jihani
+
+object function {
+   def main(args:Array[String])
+   {
+     var f=toFahrenheit(35)
+     println(f)
+     sVolume(5)
+     var C=cost(24.95f,40,60)
+     println(C)
+     
+     
+   }
+   
+   
+  def toFahrenheit(c: Int) = {
+    c*1.8+32
+    
+ 
+}
+  
+  
+  def sVolume(r:Float)={
+    val pi=4/3f
+    var v=pi*r*r*r
+    println(v)
+  }
+  
+  def cost(c:Float,d:Float,n:Int)={
+    (c-(c*d/100))*n+((n-50)*0.75)+50*3
+    
+    
+    
+  }
+
+}
